a5cf656cd745e39f605ef1ef9d322b4895a8717f
dimpleyadav <dimpleyadav32@gmail.com>
2024-03-16 21:26:09 +0530

    responsive

--- src/app/caption/page.tsx
+++ src/app/caption/page.tsx
@@ -1,14 +1,20 @@
 import Link from 'next/link';
-export default function caption(){
+
+export default function Caption() {
     return (
         <>
-        <div className="relative">
-          <div className="absolute w-full"><video className="w-full  " autoPlay muted>
-            <source src="/images/table.mp4" type="video/mp4" />
-          </video></div>
-          <div className="absolute left-[44%] top-[85vh] "> <Link href={"/caption/craft1"}><button className="minecraft-btn mx-auto w-64 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Enchant</button></Link></div>
-          
-        </div>
+            <div className="relative w-full h-screen">
+                <div className="absolute inset-0 overflow-hidden">
+                    <video className="w-full h-full object-cover" autoPlay muted>
+                        <source src="/images/table.mp4" type="video/mp4" />
+                    </video>
+                </div>
+                <div className="absolute left-1/2 bottom-8 transform -translate-x-1/2">
+                    <Link href="/caption/craft1">
+                        <button className="minecraft-btn w-64 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Enchant</button>
+                    </Link>
+                </div>
+            </div>
         </>
     )
 }

f6000358d63267b5d3f0197e4e92e0db4d0f7ac4
dimpleyadav <dimpleyadav32@gmail.com>
2024-03-16 19:55:13 +0530

    commit

--- src/app/caption/craft1/page.tsx
+++ src/app/caption/craft1/page.tsx
@@ -1,39 +1,40 @@
 "use client"
-
+import React, { useState } from 'react';
 import YouTubePlayer from "../../../../components/YoutubePlayer";
-import React, { ChangeEvent, useState, useEffect } from 'react';
+import axios from 'axios';
 
-export default function Craft(){
+export default function Craft() {
     const [url, setUrl] = useState<string>('');
     const [Videourl, setVideoUrl] = useState<string>('');
     const [transcript, setTranscript] = useState<string>('');
-  
+
     const handle = (e: React.ChangeEvent<HTMLInputElement>) => {
-      setUrl(e.target.value)
+        setUrl(e.target.value)
     }
 
     const buttonClick = async () => {
-      try {
-        const response = await fetch('/api/youtube', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ youtubeLink: url }),
-        });
-        if (!response.ok) {
-          throw new Error('Failed to download video');
+        try {
+            const response = await fetch('/api/youtube', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ youtubeLink: url }),
+            });
+            if (!response.ok) {
+                throw new Error('Failed to download video');
+            }
+            const data = await response.json();
+            console.log('Video downloaded:', data.filePath);
+        } catch (error) {
+            console.error('Error downloading video:', error);
         }
-        const data = await response.json();
-        console.log('Video downloaded:', data.filePath);
-      } catch (error) {
-        console.error('Error downloading video:', error);
-      }
     };
 
     const previewButton = () => {
-      setVideoUrl(url)
-    }
+        setVideoUrl(url)
+    };
+
     const fetchTranscript = async () => {
         try {
             const response = await axios.post('/api/whisperai/transcript', { url: Videourl });
@@ -45,43 +46,43 @@ export default function Craft(){
 
     return (
         <>
-        <div className='bg-[url("/images/dirtbgg.png")]'>
-
-        <div className="flex flex-col gap-12 ]">
-            <div className="flex justify-center items-center mt-16 gap-4">
-                <div className="w-[80%]  flex justif-center items-center ">
-                    <input
-                        value={url}
-                        onChange={handle}
-                        placeholder="Paste URL here"
-                        type="text"
-                        className=" h-12 w-[100%]  bg-[#5E5E5E] border border-white text-white"
-                    />
-                </div>
-                <div className="flex justify-center items-center">
-                    <button
-                        onClick={() => { previewButton(); buttonClick(); }}
-                        className="flex justify-center items-center minecraft-btn w-12 h-12 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200"
-                    >
-                        <img className="scale-[200%]" src="/images/go.png" alt="Go" />
-                    </button>
-                </div>
-            </div>
-            <div className="w-[83%] mx-auto flex justify-center items-center"><div className="relative w-[100%] h-72 xl:h-[70vh] lg:h-[70vh] md:h-[60vh] border border-white bg-[#5E5E5E]"></div>
-                {Videourl && (
-                    <div className="absolute left-[35%] top-[31%] z-5">
-                        <div className="flex flex-col gap-5">
-                            <YouTubePlayer url={Videourl} />
+            <div className='bg-[url("/images/dirtbgg.png")]'>
+                <div className="flex flex-col gap-12 ]">
+                    <div className="flex justify-center items-center mt-16 gap-4">
+                        <div className="w-[80%]  flex justif-center items-center ">
+                            <input
+                                value={url}
+                                onChange={handle}
+                                placeholder="Paste URL here"
+                                type="text"
+                                className="h-12 w-[100%]  bg-[#5E5E5E] border border-white text-white"
+                            />
+                        </div>
+                        <div className="flex justify-center items-center">
+                            <button
+                                onClick={() => { previewButton(); buttonClick(); }}
+                                className="flex justify-center items-center minecraft-btn w-12 h-12 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200"
+                            >
+                                <img className="scale-[200%]" src="/images/go.png" alt="Go" />
+                            </button>
                         </div>
                     </div>
-                )}
-            </div>
-            <div className="flex justify-center items-center">
-                <button onClick={fetchTranscript} className="minecraft-btn h-8 w-32 text-center text-white text-[0.5rem] truncate p-2 border-2 border-b-4 hover:text-yellow-200">
-                    Generate Transcript
-                </button>
-            </div>
-            {transcript && (
+                    <div className="w-[83%] mx-auto flex justify-center items-center">
+                        <div className="relative w-[100%] h-72 xl:h-[70vh] lg:h-[70vh] md:h-[60vh] border border-white bg-[#5E5E5E]"></div>
+                        {Videourl && (
+                            <div className="absolute left-[35%] top-[31%] z-5">
+                                <div className="flex flex-col gap-5">
+                                    <YouTubePlayer url={Videourl} />
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <button onClick={fetchTranscript} className="minecraft-btn h-8 w-32 text-center text-white text-[0.5rem] truncate p-2 border-2 border-b-4 hover:text-yellow-200">
+                            Generate Transcript
+                        </button>
+                    </div>
+                    {transcript && (
                         <div className="flex justify-center items-center">
                             <div className="transcript-div">
                                 <h2>Transcript:</h2>
@@ -89,11 +90,12 @@ export default function Craft(){
                             </div>
                         </div>
                     )}
-            <div className="flex justify-center h-[40vh] w-[100%] xl:h-[120vh] lg:h-[90vh] lg:w-[98%] md:h-[80vh] md:w-[100vw] sm:h-[50vh] sm:w-[80vh] items-center ">
-                <img src="/images/transcript.png" alt="Transcript" className="size-[80%]  xl:size-[80%] lg:size-[70%] md:size-[70%] sm:size-[70%]" />
+                    <div className="flex justify-center h-[40vh] w-[100%] xl:h-[120vh] lg:h-[90vh] lg:w-[98%] md:h-[80vh] md:w-[100vw] sm:h-[50vh] sm:w-[80vh] items-center ">
+                        <img src="/images/transcript.png" alt="Transcript" className="size-[80%]  xl:size-[80%] lg:size-[70%] md:size-[70%] sm:size-[70%]" />
+                    </div>
+                </div>
             </div>
-        </div>
-        </div>
         </>
     );
 }
+
--- src/app/caption/page.tsx
+++ src/app/caption/page.tsx
@@ -6,7 +6,7 @@ export default function caption(){
           <div className="absolute w-full"><video className="w-full  " autoPlay muted>
             <source src="/images/table.mp4" type="video/mp4" />
           </video></div>
-          <div className="absolute left-[44%] top-[85vh] "> <Link href={"/caption/craft"}><button className="minecraft-btn mx-auto w-64 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Enchant</button></Link></div>
+          <div className="absolute left-[44%] top-[85vh] "> <Link href={"/caption/craft1"}><button className="minecraft-btn mx-auto w-64 text-center text-white truncate p-1 border-2 border-b-4 hover:text-yellow-200">Enchant</button></Link></div>
           
         </div>
         </>
